ddcea92a2462b11f4de12cf2649432c47d37fdbd
nextsigner <nextsigner@gmail.com>
2020-10-17 18:40:47 -0300

    se sube todo sabado dia de la madre

--- caps.js
+++ caps.js
@@ -20,6 +20,55 @@
             }
             res.status(200).send({captura: capRegistered}) })
     };
+    getAsistencias = function(req, res){
+        console.log('Buscando capturas de administrador '+req.query.consulta)
+        //var regExp= new RegExp(''+(''+req.query.consulta).toUpperCase()+'|'+(''+req.query.consulta).toLocaleLowerCase())
+        var regExp= new RegExp(''+(''+req.query.consulta).toUpperCase()+'|'+(''+req.query.consulta).toLocaleLowerCase())
+        Captura.find({
+                          //date: {$gt: h }
+                          //date: {$gte: "2019-06-12T00:00:00+01:00", $lte: "2019-12-12T23:00:00+01:00" }
+                          //date: {$gte: h, $lte: hf }
+                          administrador: regExp
+                      },
+        /*Producto.find({ $or: [ { codigo: regExp }, { descripcion: regExp } ] },*/
+                      ['administrador', 'fotografo', 'fotode', 'fechaRegistro'], // Columns to Return
+                      {
+                          skip:0, // Starting Row
+                          //limit:1, // Ending Row
+                          sort:{
+                              fechaRegistro: 1 //Sort by Date Added DESC
+                          }
+                      },
+                      function(err, resultados){
+                          if(err) res.status(500).send({mensaje: `Error al buscar capturas: ${err}`})
+                          if(resultados.length===0){
+                              console.log('No se encontró ninguna captura '+req.query.consulta);
+                              res.status(200).send({asistencias: false})
+                          }else{
+                              console.log('Se devolverán datos de capturas a la consulta: '+req.query.consulta);
+                              res.status(200).send({asistencias: resultados})
+                          }
+                      })
+    };
+    getImagen = function(req, res){
+        console.log('Buscando capturas con ide '+req.query.id)
+        //var regExp= new RegExp(''+(''+req.query.consulta).toUpperCase()+'|'+(''+req.query.consulta).toLocaleLowerCase())
+        Captura.findById(req.query.id,
+                         ['imagen'],
+                      function(err, resultados){
+                          if(err) res.status(500).send({mensaje: `Error al buscar capturas: ${err}`})
+                          if(resultados.length===0){
+                              console.log('No se encontró ninguna captura '+req.query.id);
+                              res.status(200).send({asistencias: false})
+                          }else{
+                              console.log('Se devolverán datos de capturas a la consulta: '+req.query.id);
+                              console.log('::::: '+resultados);
+                              res.status(200).send({asistencias: resultados})
+                          }
+                      })
+    };
     app.post('/fotocapp/nuevacaptura', nuevaCaptura);
+    app.get('/fotocapp/getAsistencias', getAsistencias);
+    app.get('/fotocapp/getImagen', getImagen);
 }
 

1605749fb0af278b744dceec21473cdab5789ede
nextsigner <nextsigner@gmail.com>
2020-10-25 17:40:03 -0300

    se cambia a puerto 83 y 84

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var appServerName='fotocapp'
 var args = process.argv.slice(2);
-var puertoApp = '8080'; //Puerto que recibe acciones
-var puertoStatico = '8081'; //Puerto para descargar archivos
+var puertoApp = '8083'; //Puerto que recibe acciones
+var puertoStatico = '8084'; //Puerto para descargar archivos
 var folderFiles = 'files/';
 var serverEmail=process.env.EMAIL
 var serverEmailPass=process.env.EMAILPASS
